2dbe5dc46e26142981f77a50d9c672a9cf2a60a4
wangyuchuan <wang.yuchuan@etaoshi.com>
2015-03-03 18:47:49 +0800

    add login code.

---
+++ src/wychuan/wychuan.com/Controllers/AdminController.cs
@@ -0,0 +1,29 @@
+﻿using System.Web.Mvc;
+using AC.Web.Models;
+
+namespace AC.Web.Controllers
+{
+    [Authorize]
+    public class AdminController : Controller
+    {
+        /// <summary>
+        /// 登录
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult Login(LoginModel loginModel)
+        {
+            return View("Index");
+        }
+
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
